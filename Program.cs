c177c09ebeae0f3bb953cd9a1bb130ac98c70b1f
SamirDev69 <aounsamir2017@gmail.com>
2022-03-06 21:04:23 +0100

    yes com

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace RedirectApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var prs = new ProcessStartInfo("chrome");
+            prs.Arguments = "https://youtu.be/-WSBK_NePew";
+            Process.Start(prs);
+        }
+    }
+}
